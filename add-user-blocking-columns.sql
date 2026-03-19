ce9954c74c3cd9924a0ff55c15b96876fc09f066
Jeremy Sendon <jemsednon.dmibpo@gmail.com>
2026-03-19 09:57:58 +0800

    Done

--- add-user-blocking-columns.sql
+++
@@ -1,7 +0,0 @@
-alter table public.users
-add column if not exists is_blocked boolean not null default false,
-add column if not exists blocked_at timestamptz null;
-
-update public.users
-set is_blocked = false
-where is_blocked is null;
